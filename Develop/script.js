e7ee23588af77eb99de0a92882d1304253fa1e25
alen <alen.dunn1@gmail.com>
2022-01-12 19:17:39 -0500

    added some notes and a starting function

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,26 @@
 // Assignment code here
 
+// when generate button is clicked, window prompts appear to determine password criteria 
+function generatePassword() {
+  window.alert("Please choose password criteria.");
+
+};
+
+
+// window prompt for length of pass (8 min, 128 max)
+    
+// window prompt to use lowercase characters
+  //if TRUE - use lowercase
+
+// window prompt to use uppercase characters
+  // else use uppercase
+// window prompt to use numeric chars
+  // if TRUE - use numeric
+  // else, dont use numeric
+// window prompt to use special chars
+  // if TRUE - use special chars
+  //  else, dont use special chars
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
